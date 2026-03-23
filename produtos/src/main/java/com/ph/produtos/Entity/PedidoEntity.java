159380551a39116ec4149dd0328f7768e93dcbef
PauloMalavasi <paulomalavasi44@gamil.com>
2026-03-22 22:14:25 -0300

    Entity

---
+++ produtos/src/main/java/com/ph/produtos/Entity/PedidoEntity.java
@@ -0,0 +1,56 @@
+package com.ph.produtos.Entity;
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_pedido")
+public class PedidoEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "dataPedido")
+    private LocalDateTime data;
+    @Column(name = "valorTotalPedido")
+    private BigDecimal valorTotal;
+    private String status;
+
+    public PedidoEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    public void setData(LocalDateTime data) {
+        this.data = data;
+    }
+
+    public BigDecimal getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
